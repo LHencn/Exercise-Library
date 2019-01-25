9991ee88c47e720a9daea40ff39396f9bcd4ee2b
lhc <3115629644@qq.com>
2019-01-25 15:40:55 +0800

    AVL树

--- data_structure/16.AVL.c
+++ data_structure/16.AVL.c
@@ -4,22 +4,22 @@
 	> Mail: 3115629644@qq.com 
 	> Created Time: 2019年01月25日 星期五 15时06分27秒
  ************************************************************************/
-
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#define max(a, b) {(\
-    __typeof(a) __a = (a);\
-    __typeof(b) __b = (b);\
-    __a > __b ? __a : __b;\
-                   )}
+
+#define max(a, b) ({ \
+    __typeof(a) __a = (a); \
+    __typeof(b) __b = (b); \
+    __a > __b ? __a : __b; \
+})
 
 typedef struct Node {
     int key;
     int height;
     struct Node *lchild, *rchild;
-}Node;
+} Node;
 
-Node *NIL; //虚拟节点高度为0
+Node *NIL = NULL;
 
 Node *init(int key) {
     Node *p = (Node *)calloc(sizeof(Node), 1);
@@ -29,7 +29,7 @@ Node *init(int key) {
     return p;
 }
 
-__attribute__((construtor))
+__attribute__((constructor))
 void init_NIL() {
     NIL = init(0);
     NIL->height = 0;
@@ -39,20 +39,26 @@ void init_NIL() {
 
 void calc_height(Node *root) {
     root->height = max(root->lchild->height, root->rchild->height) + 1;
+    return ;
 }
+
 Node *left_rotate(Node *root) {
     Node *temp = root->rchild;
     root->rchild = temp->lchild;
     temp->lchild = root;
-
+    calc_height(root);
+    calc_height(temp);
+    return temp;
 }
 
 Node *right_rotate(Node *root) {
     Node *temp = root->lchild;
-    root->rchild = temp->lchild;
-    temp->lchild = root;
+    root->lchild = temp->rchild;
+    temp->rchild = root;
+    calc_height(root);
+    calc_height(temp);
+    return temp;
 }
-
 
 Node *maintain(Node *root) {
     if (abs(root->lchild->height - root->rchild->height) <= 1) return root;
@@ -62,20 +68,27 @@ Node *maintain(Node *root) {
         }
         root = right_rotate(root);
     } else {
-        
+        if (root->rchild->rchild->height < root->rchild->lchild->height) {
+            root->rchild = right_rotate(root->rchild);
+        }
+        root = left_rotate(root);
     }
+    return root;
 }
 
 Node *insert(Node *root, int key) {
-    if (root == NULL) return init(key);
-    if (root->key = key) return root;
+    if (root == NIL) return init(key);
+    if (root->key == key) return root;
     else if (root->key < key) root->rchild = insert(root->rchild, key);
     else root->lchild = insert(root->lchild, key);
+    calc_height(root);
     return maintain(root);
 }
 
-Node *predecossor(Node *root) {
-    Node *temp = 
+Node *predecessor(Node *root) {
+    Node *temp = root->lchild;
+    while (temp->rchild != NIL) temp = temp->rchild;
+    return temp;
 }
 
 Node *erase(Node *root, int key) {
@@ -93,7 +106,7 @@ Node *erase(Node *root, int key) {
             free(root);
             return temp;
         } else {
-            Node *temp = predecossor(root);
+            Node *temp = predecessor(root); 
             root->key = temp->key;
             root = erase(root->lchild, temp->key);
         }
@@ -103,7 +116,7 @@ Node *erase(Node *root, int key) {
 }
 
 void clear(Node *node) {
-    if (node == NULL) return ;
+    if (node == NIL) return ;
     clear(node->lchild);
     clear(node->rchild);
     free(node);
@@ -122,12 +135,11 @@ int main() {
     int op, val;
     Node *root = NIL;
     while (scanf("%d%d", &op, &val) != EOF) {
-        switch(op) {
-            case 1: insert(root, val); break;
-            case 2: erase(root, val); break;
+        switch (op) {
+            case 1: root = insert(root, val); break;
+            case 2: root = erase(root, val); break;
         }
         output(root);
     }
-
     return 0;
 }
