f77ae0b00db5bd7dba59072b50514e43df19aade
lhc <3115629644@qq.com>
2019-03-08 20:18:22 +0800

    vector

---
+++ C_C++/2._vector.cpp
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name: 2._vector.cpp
+	> Author: LHC 
+	> Mail: 3115629644@qq.com 
+	> Created Time: 2019年03月08日 星期五 12时27分44秒
+ ************************************************************************/
+
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main() {
+    vector<int> arr(14);
+    arr[0] = 1;
+    arr[2] = 2;
+    cout << arr.size() << endl;
+
+
+    return 0;
+}
